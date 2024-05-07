aff22305f73049ddf36d2c20a120391a49022865
asfasfasfas121 <bsjcurakbaba038@hotmail.com>
2024-05-07 10:33:33 +0700

    demo project

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const factorial = (n) => {
-  if (n === 0 || n === 1) {
-    return 1;
+function largestDivisibleSubset(nums) {
+  nums.sort((a, b) => a - b);
+  const dp = new Array(nums.length).fill(1);
+  let maxSubsetSize = 1;
+  let maxSubsetIdx = 0;
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] % nums[j] === 0) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+        if (dp[i] > maxSubsetSize) {
+          maxSubsetSize = dp[i];
+          maxSubsetIdx = i;
+        }
+      }
+    }
   }
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
+  const result = [];
+  let prev = nums[maxSubsetIdx];
+  let count = maxSubsetSize;
+  for (let i = maxSubsetIdx; i >= 0; i--) {
+    if (prev % nums[i] === 0 && dp[i] === count) {
+      result.unshift(nums[i]);
+      prev = nums[i];
+      count--;
+    }
   }
   return result;
-};
+}
