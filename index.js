df4a440dd6ce5b8739f4fa0688d4c1723ef4a655
asfasfasfas121 <bsjcurakbaba038@hotmail.com>
2024-05-09 08:33:33 +0700

    update aptos task

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
+function isValidBST(root) {
+  let prev = null;
+  return inorder(root);
+  function inorder(node) {
+    if (!node) return true;
+    if (!inorder(node.left)) return false;
+    if (prev !== null && node.val <= prev) return false;
+    prev = node.val;
+    return inorder(node.right);
+  }
+}
