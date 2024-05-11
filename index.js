b63aa64628757f89f29d5cd970f5dba8fd9ac871
asfasfasfas121 <bsjcurakbaba038@hotmail.com>
2024-05-11 09:02:04 +0700

    feat project commit

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-function numIslands(grid) {
-  if (grid.length === 0 || grid[0].length === 0) return 0;
-  const rows = grid.length;
-  const cols = grid[0].length;
-  let islands = 0;
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (grid[i][j] === "1") {
-        islands++;
-        dfs(i, j);
-      }
-    }
+function mergeKLists(lists) {
+  if (!lists || !lists.length) return null;
+  return mergeLists(lists, 0, lists.length - 1);
+  function mergeLists(lists, start, end) {
+    if (start === end) return lists[start];
+    const mid = Math.floor((start + end) / 2);
+    const left = mergeLists(lists, start, mid);
+    const right = mergeLists(lists, mid + 1, end);
+    return mergeTwoLists(left, right);
   }
-  return islands;
-  function dfs(row, col) {
-    if (
-      row < 0 ||
-      row >= rows ||
-      col < 0 ||
-      col >= cols ||
-      grid[row][col] !== "1"
-    )
-      return;
-    grid[row][col] = "0";
-    dfs(row + 1, col);
-    dfs(row - 1, col);
-    dfs(row, col + 1);
-    dfs(row, col - 1);
+  function mergeTwoLists(l1, l2) {
+    if (!l1) return l2;
+    if (!l2) return l1;
+    if (l1.val < l2.val) {
+      l1.next = mergeTwoLists(l1.next, l2);
+      return l1;
+    } else {
+      l2.next = mergeTwoLists(l1, l2.next);
+      return l2;
+    }
   }
 }
