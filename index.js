0da40a05c3cec053583a69404e57a3d6fbf8af44
asfasfasfas121 <bsjcurakbaba038@hotmail.com>
2024-05-12 17:12:00 +0700

    update aptos commit

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-function mergeKLists(lists) {
-  if (!lists || !lists.length) return null;
-  return mergeLists(lists, 0, lists.length - 1);
-  function mergeLists(lists, start, end) {
-    if (start === end) return lists[start];
-    const mid = Math.floor((start + end) / 2);
-    const left = mergeLists(lists, start, mid);
-    const right = mergeLists(lists, mid + 1, end);
-    return mergeTwoLists(left, right);
-  }
-  function mergeTwoLists(l1, l2) {
-    if (!l1) return l2;
-    if (!l2) return l1;
-    if (l1.val < l2.val) {
-      l1.next = mergeTwoLists(l1.next, l2);
-      return l1;
-    } else {
-      l2.next = mergeTwoLists(l1, l2.next);
-      return l2;
-    }
-  }
-}
+// Fund Alice and sponsor accounts
+await aptos.fundAccount({
+  accountAddress: aliceAddress,
+  amount: ALICE_INITIAL_BALANCE,
+});
+await aptos.fundAccount({
+  accountAddress: sponsorAddress,
+  amount: SPONSOR_INITIAL_BALANCE,
+});
+
+// Show account balances
+const aliceBalanceBefore = await aptos.getAccountCoinsData({
+  accountAddress: aliceAddress,
+});
+const sponsorBalanceBefore = await aptos.getAccountCoinsData({
+  accountAddress: sponsorAddress,
+});
